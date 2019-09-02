5ce7995f15f25b2b3224478fcbd3b54911144e9b
xiongwei <2894001817@qq.com>
2019-09-02 23:29:41 +0800

    做这个东西真的很烦，一上来就直接使用vue的方式而不是HTML引入的方式，感觉无从下手

---
+++ todo-list/src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="todo-list-example">
+    <form v-on:submit.prevent="addNewTodo">
+      <label for="new-todo">Add a todo</label>
+      <input v-model="newTodoText" id="new-todo" placeholder="添加一个选项" />
+      <button>Add</button>
+    </form>
+    <ul>
+      <li
+        v-for="(todo, index) in todos"
+        v-bind:key="todo.id"
+        v-bind:title="todo.title"
+        v-on:remove="todos.splice(index, 1)"
+      ><input type="checkbox" id="checkbox">
+         <label for="checkbox">{{ todo.title}}</label>
+      </li> 
+    </ul>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    
+  },
+  data() {
+    return {
+      newTodoText: "",
+      todos: [
+       
+      ],
+      nextTodoId: 4
+    };
+  },
+  methods: {
+    addNewTodo: function() {
+      this.todos.push({
+        id: this.nextTodoId++,
+        title: this.newTodoText
+      });
+      this.newTodoText = "";
+    },
+  },
+  mounted: function() {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+button {
+  margin: 0 20px;
+}
+li{list-style:none;}
+</style>
